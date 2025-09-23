a898dc12d6ac1bf814b6ebebe86531a9df527e32
David Pocar <pocar.david@gmail.com>
2025-09-23 12:09:16 +0200

    require Symfony 4.4

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -36,8 +36,8 @@ class Configuration implements \Symfony\Component\Config\Definition\Configuratio
 
 	public function getConfigTreeBuilder(): TreeBuilder
 	{
-		$treeBuilder = new TreeBuilder();
-		$rootNode = $treeBuilder->root($this->rootNode);
+		$treeBuilder = new TreeBuilder($this->rootNode);
+		$rootNode = $treeBuilder->getRootNode();
 
 		$rootNode
 			->children()
